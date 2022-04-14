7abbcfef79a66d172e89263dea1c6ac65f6dde1a
Yosafat <77567907+crypter7054@users.noreply.github.com>
2022-04-14 09:18:05 +0700

    update

--- memoryMap.c
+++ memoryMap.c
@@ -9,76 +9,93 @@
 
 int main(int argc, char **argv) {
 
-    int pid;    
     int size = MSGSIZE * sizeof(int);
     
     //buat memory map
     void *addr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    //printf("Mapped at %p\n", addr);
     int *shared = addr;
     int *shared_idx = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     *shared_idx = 0;
+    int *terminate = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    *terminate = 0;
 
     // input jumlah proses berdasarkan masukkan user
-    printf("Masukkan jumlah fork proses :\n");
-    int count;
-    scanf("%d", &count);
+    printf("Masukkan jumlah fork proses produsen : ");
+    int countp;
+    scanf("%d", &countp);
+    *terminate = countp;
+    printf("Masukkan jumlah fork proses konsumen : ");
+    int countk;
+    scanf("%d", &countk);
 
-    // looping jumlah fork proses
-    for (int j = 0; j < count; j++)
-    {
-        pid = fork();
-    }
+    int pid_main = fork();
+    switch (pid_main) {
+    case 0:         /*  proses produce */
 
-    switch (pid) {
-    case 0:         /*  proses anak */
+        // lakukan fork sebanyak inputan user
+        for (int p = 1; p < countp; p++){
+            fork();
+        }
+
+        //sleep(5);
         printf("Proses Produce \n");
         srand(getpid()); //seed random berdasarkan pid
 
-        for (int i = 0; i < 5; i++)
-        {
+        for (int i = 0; i < 1; i++){
             shared[*shared_idx] = rand() % 10;                 // tulis data angka random ke pipe 
             printf("produsen (pid:%d) menghasilkan: %d\n", getpid(), shared[*shared_idx]);
             *shared_idx += 1;
             sleep(1);
         }
         
-        
+        *terminate = *terminate - 1;
         break;
-    default:        /*  ortu */
-        printf("Proses Consume \n");
-        int pidWait, status, total_konsum = 0;
+    default:        /*  consume */
+        printf("\n");
+        int pid_c = fork();
+        switch(pid_c){
+        case 0:
+            // lakukan fork sebanyak inputan user
+            for (int k = 1; k < countk; k++){
+                fork();
+            }
+            
+            printf("Proses Consume \n");
+            int total_konsum = 0;
 
-        /* wait sampai child selesai */
-        while (pidWait = wait(&status)) {   
-
-            if (pidWait == pid)  /* child sukses selesai*/
-
-                //cetak isi memory map
-                for (int i = 0; i<MSGSIZE; i++) {
+            //cetak isi memory map
+            for (int i = 0; i<1; i++) {
+            
+                if(*shared_idx != 0){
                     printf("konsumen (pid:%d) mengambil: %d \n", getpid(), shared[*shared_idx - 1]);
                     total_konsum += shared[*shared_idx - 1];      // jumlahkan angka
                     *shared_idx -= 1;
-                    sleep(1);
+                    
+                }else{
+                    if(*terminate != 0){
+                        while((*shared_idx == 0) && (*terminate != 0)){
+                            //printf("waiting...\n");
+                            sleep(1);
+                        }
+                        i--;
+                    }
                 }
-                break; //keluar dari loop wait
-
-            if ((pidWait == -1) && (errno != EINTR)) {
-                /* ada error*/
-                perror("waitpid");
-                exit(1);
+            
+                sleep(1);
             }
+            // tampilkan hasil penjumlahan
+            printf("total konsumsi konsumen (pid:%d) : %d \n", getpid(),total_konsum);
+            break;
+        
+        default:
+            wait(NULL);
+            break;
         }
-        // tampilkan hasil penjumlahan
-        printf("total konsumsi komsumen (pid:%d) : %d \n", getpid(),total_konsum);
+        
         break;
-
-    case -1:        /* error */
-        perror("fork");
-        exit(1);
     }
+
     exit(0);
 
     return 0;
 }
-
