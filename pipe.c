8cc3a85d8e1281d9cb9657d045c451999c5ebe08
Yosafat <77567907+crypter7054@users.noreply.github.com>
2022-04-14 08:37:52 +0700

    update pipe

--- pipe.c
+++ pipe.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>     /* exit() */
 #include <unistd.h>     /* fork() and getpid() */
 #include <stdio.h>      /* printf() */
-#include <string.h>
+#include <sys/wait.h> /*wait*/
 
 int main(int argc, char **argv) {
 
-    int pid;
     int fd[2];  // pipe
 
     // create pipe
@@ -14,22 +13,24 @@ int main(int argc, char **argv) {
     }
 
     // input jumlah proses berdasarkan masukkan user
-    printf("Masukkan jumlah fork proses :\n");
-    int count;
-    scanf("%d", &count);
+    printf("Masukkan jumlah fork proses produsen : ");
+    int countp;
+    scanf("%d", &countp);
+    printf("Masukkan jumlah fork proses konsumen : ");
+    int countk;
+    scanf("%d", &countk);
+    
 
-    // looping jumlah proses
-    for (int j = 0; j < count; j++)
-    {
-        pid = fork();
-    }
-
-
-    switch (pid) {
+    int pid_main = fork();
+    switch (pid_main){
     case 0: /* fork returns 0 ke proses anak */
+
+        // lakukan fork sebanyak inputan user
+        for (int p = 1; p < countp; p++){
+            fork();
+        }
         
         printf("Proses Produce \n");
-        int i;
         int msg1;
         
         /* tutup bagian input dari pipe */
@@ -38,7 +39,7 @@ int main(int argc, char **argv) {
         srand(getpid()); //seed random berdasarkan pid
 
         /* tulis ke pipe */
-        for(i=0; i<5; i++){
+        for(int i=0; i<2; i++){
             msg1 = rand() % 10;                 // tulis data angka random ke pipe 
             write(fd[1], &msg1, sizeof(msg1));  // tulis ke pipe
             printf("produsen (pid:%d) menghasilkan: %d\n", getpid(), msg1);
@@ -47,28 +48,42 @@ int main(int argc, char **argv) {
         break;
 
     default: /* fork returns pid ke proses ortu */
-        
-        printf("Proses Consume \n");
-        int total_konsum = 0;
-        int y;
-        
-        /* tutup bagian output dari pipe */
-        close(fd[1]);
-        
-        /* baca yang ditulis child dari pipe */
-        for(i=0; i<5; i++){
-            read(fd[0], &y, sizeof(y)); 
-            printf("konsumen (pid:%d) mengambil: %d \n", getpid(), y);
-            total_konsum += y;      // jumlahkan angka
-            sleep(1);
-        }
-        // tampilkan hasil penjumlahan
-        printf("total konsumsi komsumen (pid:%d) : %d \n", getpid(),total_konsum);
-        break;
+        printf("\n");
+        int pid_c = fork();
 
-    case -1: /* error */
-        perror("fork");
-        exit(1);
+        switch(pid_c){
+        case 0:
+
+            // lakukan fork sebanyak inputan user
+            for (int k = 1; k < countk; k++){
+                fork();
+            }
+
+
+            printf("Proses Consume \n");
+            int total_konsum = 0, y;
+            
+
+            /* tutup bagian output dari pipe */
+            close(fd[1]);
+            
+            /* baca yang ditulis child dari pipe */
+            for(int i=0; i<2; i++){
+                read(fd[0], &y, sizeof(y)); 
+                printf("konsumen (pid:%d) mengambil: %d \n", getpid(), y);
+                total_konsum += y;      // jumlahkan angka
+                sleep(1);
+            }
+            // tampilkan hasil penjumlahan
+            printf("total konsumsi konsumen (pid:%d) : %d \n", getpid(),total_konsum);
+            break;
+        
+        default:
+            // wait sampai proses child selesai
+            wait(NULL);
+            break;
+        }
+
     }
     
     exit(0);
